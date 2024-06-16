1489f8ecbd2cf53599ff38d6cb7ab33a3271513a
Geeneth Medagedara <geenethmedagedara@gmail.com>
2024-06-17 00:33:12 +0530

    Brian's First Commit: MAde changes to About Us Section.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
-import LoginRegister from './Components/LoginRegister/LoginRegister';
-import {Routes, Route, Navigate, useNavigate} from 'react-router-dom'
-import axios from 'axios';
-import MainHome from './Components/LoginRegister/MainHome';
-import RegisterPage from './Components/LoginRegister/RegisterPage';
-import { createContext, useState, useEffect} from 'react';
-import Contact from './Components/Pages/ContactPage/Contact';
-import AboutUs from './Components/Pages/AboutUsPage/AboutUs';
-
-
+import LoginRegister from "./Components/LoginRegister/LoginRegister";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
+import MainHome from "./Components/LoginRegister/MainHome";
+import RegisterPage from "./Components/LoginRegister/RegisterPage";
+import { createContext, useState, useEffect } from "react";
+import Contact from "./Components/Pages/ContactPage/Contact";
+import AboutUs from "./Components/Pages/AboutUsPage/AboutUs";
 
 export const Context = createContext();
 
@@ -15,46 +13,45 @@ axios.defaults.baseURL = "http://localhost:4000/api";
 axios.defaults.withCredentials = true;
 
 function App() {
-  const [Authenticated, setAuthenticated] = useState(false);
-  const navigate = useNavigate();
+  //   const [Authenticated, setAuthenticated] = useState(false);
+  //   const navigate = useNavigate();
 
-  useEffect(() => {
-    try {
-      axios.get('/').then((data) => {
-        // alert(data.data)
-        // console.log(data.data)
-        // if(data.status === 200) {
-        //   setAuthenticated(true)
-        //   navigate('/home'); 
-        //   console.log("this works ")
-        // }
-        if(data.data){
-          setAuthenticated(true)
-          navigate('/home'); 
-          console.log("setting auth")
-        }
-          
-          
-      });
-  
-    } catch (error) {
-      console.log("error occc"+ error)
-    } 
-},[]); 
+  //   useEffect(() => {
+  //     try {
+  //       axios.get('/').then((data) => {
+  //         // alert(data.data)
+  //         // console.log(data.data)
+  //         // if(data.status === 200) {
+  //         //   setAuthenticated(true)
+  //         //   navigate('/home');
+  //         //   console.log("this works ")
+  //         // }
+  //         if(data.data){
+  //           setAuthenticated(true)
+  //           navigate('/home');
+  //           console.log("setting auth")
+  //         }
+
+  //       });
+
+  //     } catch (error) {
+  //       console.log("error occc"+ error)
+  //     }
+  // },[]);
   return (
-    <div >
-      <Context.Provider value={[Authenticated, setAuthenticated]}>
+    <div>
+      {/* <Context.Provider value={[Authenticated, setAuthenticated]}> */}
       <Routes>
-      {/* {Authenticated ? (
+        {/* {Authenticated ? (
           <Route path='/home' element={<MainHome/>}/>
         ) : (
           <Route path='/' element={<LoginRegister/>}/>
         )} */}
-          <Route path='/home' element={Authenticated ? <MainHome /> : <Navigate to="/" />}/>
-          <Route path='/' element={<AboutUs/>}/>
-          <Route path='/register' element={<RegisterPage/>}/>
-        </Routes>
-        </Context.Provider>
+        {/* <Route path='/home' element={Authenticated ? <MainHome /> : <Navigate to="/" />}/> */}
+        <Route path="/" element={<AboutUs />} />
+        {/* <Route path='/register' element={<RegisterPage/>}/> */}
+      </Routes>
+      {/* </Context.Provider> */}
     </div>
   );
 }
