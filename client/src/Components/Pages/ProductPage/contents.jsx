4200ed2b2a5f15ca7b79e2a6fff58af38b1b26ac
kavinda dissanayake <121952332+kavinda44@users.noreply.github.com>
2024-08-06 22:29:27 +0530

    change content page

--- client/src/Components/Pages/ProductPage/contents.jsx
+++ client/src/Components/Pages/ProductPage/contents.jsx
@@ -4,18 +4,19 @@ const productData = [
       id: 1,
       name: "3D Chess",
       image:
-        "https://w7.pngwing.com/pngs/267/462/png-transparent-chess-3d-icon-thumbnail.png",
+        "https://i.imgur.com/99Fl2qe.jpg ",
       productType: "openBackHeadphones",
       price: 1,
       rating: 5,
       timeLeft: 27,
-      totalSales: 7479 
+      totalSales: 7479
+    
     },
     {
       id: 2,
       name: "crystal 3D printing Robort",
       image:
-        "https://e7.pngegg.com/pngimages/393/472/png-clipart-3d-printing-3d-printers-the-ups-store-printer-electronics-3d-computer-graphics.png",
+        "https://i.imgur.com/BhW75Mt.jpg",
       productType: "openBackHeadphones",
       price: 16,
       rating: 5,
@@ -26,12 +27,27 @@ const productData = [
       id: 3,
       name: "Liquid crystal 3D printing Groot",
       image:
-        "https://e7.pngegg.com/pngimages/641/378/png-clipart-liquid-crystal-3d-printing-groot-printer-electronics-3d-printing-thumbnail.png",
+        "https://i.imgur.com/dDCUnVX.jpg",
       productType: "openBackHeadphones",
       price: 5,
       rating: 4,
       timeLeft: 21,
       totalSales: 6347 
+
+    
+    },
+
+    {
+      id: 4,
+      name: "Moter part",
+      image:
+        "https://i.imgur.com/hkbO4il.jpg",
+      productType: "openBackHeadphones",
+      price: 4,
+      rating: 4,
+      timeLeft: 21,
+      totalSales: 637 
+
     } 
   ];
   export default productData;
