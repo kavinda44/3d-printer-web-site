92852447621dbe033faeab303f4b861facf45997
Dominic Brian <dominic91960@gmail.com>
2024-08-12 00:32:38 +0530

    Remade the Navbar.

--- client/src/Components/LoginRegister/MainHome.jsx
+++ client/src/Components/LoginRegister/MainHome.jsx
@@ -1,70 +1,68 @@
-import React, {useContext, useEffect, useState } from 'react'
-import NavBar from '../NavBar/NavBar'
-import Hero from '../Hero/Hero'
-import Home from '../Pages/HomePage/Home'
-import About from '../Pages/AboutPage/About'
+import React, { useContext, useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Context } from '../../App'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { Context } from "../../App";
+
+import NavBar from "../NavBar/NavBar";
+import Hero from "../Hero/Hero";
+import Home from "../Pages/HomePage/Home";
+import About from "../Pages/AboutPage/About";
 
 function MainHome() {
   const [user, setUser] = useState(null);
   const [Authenticated, setAuthenticated] = useContext(Context);
   const navigate = useNavigate();
 
-//   useEffect(() => {
-//     try {
-//       axios.get('/').then((data) => {
-//         // alert(data.data)
-//         // console.log(data.data)
-//         // if(data.status === 200) {
-//         //   setAuthenticated(true)
-//         //   navigate('/home'); 
-//         //   console.log("this works ")
-//         // }
-//         if(data.data){
-//           setAuthenticated(true)
-//           navigate('/home'); 
-//           console.log("setting auth main")
-//         }
-          
-          
-//       });
-  
-//     } catch (error) {
-//       console.log("error occc"+ error)
-//     } 
-// },[]); 
-  
-// const clikc = () => {
-//   axios.get('/').then((data) => {
-//     // alert(data.data)
-//     // console.log(data.data)
-//     // if(data.status === 200) {
-//     //   setAuthenticated(true)
-//     //   navigate('/home'); 
-//     //   console.log("this works ")
-//     // }
-//     if(data.data){
-//       setAuthenticated(true)
-//       navigate('/home'); 
-//       console.log("setting auth2 " + data.data)
-//     }
-      
-      
-//   });
-// }
+  //   useEffect(() => {
+  //     try {
+  //       axios.get('/').then((data) => {
+  //         // alert(data.data)
+  //         // console.log(data.data)
+  //         // if(data.status === 200) {
+  //         //   setAuthenticated(true)
+  //         //   navigate('/home');
+  //         //   console.log("this works ")
+  //         // }
+  //         if(data.data){
+  //           setAuthenticated(true)
+  //           navigate('/home');
+  //           console.log("setting auth main")
+  //         }
 
-    
-console.log("once")
+  //       });
+
+  //     } catch (error) {
+  //       console.log("error occc"+ error)
+  //     }
+  // },[]);
+
+  // const clikc = () => {
+  //   axios.get('/').then((data) => {
+  //     // alert(data.data)
+  //     // console.log(data.data)
+  //     // if(data.status === 200) {
+  //     //   setAuthenticated(true)
+  //     //   navigate('/home');
+  //     //   console.log("this works ")
+  //     // }
+  //     if(data.data){
+  //       setAuthenticated(true)
+  //       navigate('/home');
+  //       console.log("setting auth2 " + data.data)
+  //     }
+
+  //   });
+  // }
+
+  console.log("once");
   return (
     <>
-    <NavBar/>
-    <Hero/>
-    <Home/>
-    <About/>
+      <NavBar />
+      <Hero />
+      <Home />
+      <About />
     </>
-  )
+  );
 }
 
-export default MainHome
+export default MainHome;
--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,86 +1,105 @@
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Context } from "../../App";
 
-// import React, {useContext} from 'react'
-// import './NavBar.css'
-// import logo from './logo.PNG'
-// import axios from 'axios'
-// import { Context } from '../../App'
-// import { Link, Navigate, useNavigate } from 'react-router-dom'
-
-// const NavBar = () => {
-
-//   const [Authenticated, setAuthenticated] = useContext(Context);
-//   const navigate = useNavigate();
-
-//   const singout = async (e) => {
-//     e.preventDefault();
-//     const ress = await axios.post('/logout');
-//     if(ress.status === 200){
-//       setAuthenticated(false)
-//       navigate('/');
-//     }
-//   }
-//   return (
-//     <nav className='maincontainer'>
-//         <img src={logo} alt="" className='logo'/>
-//         <ul>
-//             <li className='hm'><Link to={'/home'}>Home</Link></li>
-//             <li className='hm'><Link to={'/store'}>Store</Link></li>
-//             <li className='au'><Link to={'/about'}>About us</Link></li>
-//             <li className='cu'><Link to={'/contact'}>Contact us</Link></li>
-//             <li><button className='btn' onClick={singout}>Sign Out</button></li>
-//         </ul>
-//     </nav>
-//   )
-// }
-
-// export default NavBar
-
-import React, { useContext, useState, useEffect } from 'react';
-import './NavBar.css';
-import logo from './logo.PNG';
-import axios from 'axios';
-import { Context } from '../../App';
-import { Link, useNavigate } from 'react-router-dom';
+import { IoMdCart } from "react-icons/io";
+import logo from "./logo.PNG";
+import "./NavBar.css";
 
 const NavBar = () => {
-  const [, setAuthenticated] = useContext(Context); 
-  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [, setAuthenticated] = useContext(Context);
   const [visible, setVisible] = useState(true);
+  const [isToggled, setIsToggled] = useState(false);
   const navigate = useNavigate();
 
   const signout = async (e) => {
     e.preventDefault();
-    const ress = await axios.post('/logout');
+    const ress = await axios.post("/logout");
     if (ress.status === 200) {
       setAuthenticated(false);
-      navigate('/');
+      navigate("/");
     }
   };
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
-      const isVisible = prevScrollPos > currentScrollPos || currentScrollPos < 10;
-
-      setVisible(isVisible);
-      setPrevScrollPos(currentScrollPos);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [prevScrollPos]); 
-
   return (
-    <nav className={`maincontainer ${visible ? 'visible' : 'hidden'}`}>
-      <img src={logo} alt="" className='logo'/>
-      <ul>
-        <li className='hm'><Link to={'/home'}>Home</Link></li>
-        <li className='hm'><Link to={'/store'}>Store</Link></li>
-        <li className='au'><Link to={'/about'}>About us</Link></li>
-        <li className='cu'><Link to={'/contact'}>Contact us</Link></li>
-        <li><button className='btn' onClick={signout}>Sign Out</button></li>
-      </ul>
-    </nav>
+    <header>
+      <nav
+        className={`${visible ? "visible" : "hidden"} ${
+          isToggled && "navbar-bg-color"
+        }`}
+      >
+        {/* Logo */}
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        {/* Main Nav */}
+        <ul className="main-navbar">
+          <li>
+            <Link to={"/home"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/store"}>Store</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About us</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact us</Link>
+          </li>
+          <li>
+            <button className="btn" onClick={signout}>
+              Sign Out
+            </button>
+          </li>
+        </ul>
+        {/* Mobile Nav Toggle Btn */}
+        {isToggled ? (
+          <button
+            className="navbar-toggle-btn"
+            onClick={() => {
+              setIsToggled((prev) => !prev);
+            }}
+          >
+            &#10005;
+          </button>
+        ) : (
+          <button
+            className="navbar-toggle-btn"
+            onClick={() => {
+              setIsToggled((prev) => !prev);
+            }}
+          >
+            &#9776;
+          </button>
+        )}
+        {/* Mobile Nav */}
+        {isToggled && (
+          <ul className="mobile-navbar navbar-bg-color">
+            <li>
+              <Link to={"/home"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/store"}>Store</Link>
+            </li>
+            <li>
+              <Link to={"/about"}>About us</Link>
+            </li>
+            <li>
+              <Link to={"/contact"}>Contact us</Link>
+            </li>
+            <li>
+              <button className="btn" onClick={signout}>
+                Sign Out
+              </button>
+            </li>
+          </ul>
+        )}
+      </nav>
+      <div className="cart-icon">
+        <IoMdCart />
+      </div>
+    </header>
   );
 };
 
